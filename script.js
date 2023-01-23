900226620ce9ff3604c0cda745a4664052d12910
Eric Thvedt <ericsthvedt@gmail.com>
2023-01-23 12:30:00 -0800

    shading on buttons for active section

--- script.js
+++ script.js
@@ -6,16 +6,30 @@ window.onload = function(){
     }
     document.getElementById(`home-page`).style.display = "block";
     document.getElementById(`home-page`).style.height = "100%";
+    document.getElementById('home-nav').classList.add("on-page")
 }
 
 function displayContent(event) {
-    var btn = event.target.innerText;
-    var id = btn.replace(/\s+/g, '-').toLowerCase();
+    var btn = event.target;
+    var btnText = btn.innerText
+    var btns = document.getElementsByClassName("nav")
+    var id = btnText.replace(/\s+/g, '-').toLowerCase();
     var sections = document.getElementsByClassName("content");
     for (let i = 0; i<sections.length; i++){
         sections[i].style.display = "none";
         sections[i].style.height = "0%";
     }
+    for (let i = 0; i<btns.length; i++) {
+        btns[i].classList.remove("on-page");
+    }
     document.getElementById(`${id}`).style.display = "block";
-    document.getElementById(`home-page`).style.height = "100%";
+    btn.classList.add("on-page");
+}
+
+function emailButton() {
+    alert("Sorry, this is not yet implemented!")
+}
+
+function mailButton() {
+    alert("I don't think you can send physical mail over the internet.")
 }
