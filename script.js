0e22248a5b6d9c98ebef89eee9d23893605e4a9a
Eric Thvedt <ericsthvedt@gmail.com>
2023-01-23 09:46:15 -0800

    js code for hiding/showing sections of content

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+window.onload = function(){
+    var sections = document.getElementsByClassName("content");
+    for (let i = 0; i<sections.length; i++){
+        sections[i].style.display = "none";
+        sections[i].style.height = "0%";
+    }
+    document.getElementById(`home-page`).style.display = "block";
+    document.getElementById(`home-page`).style.height = "100%";
+}
+
+function displayContent(event) {
+    var btn = event.target.innerText;
+    var id = btn.replace(/\s+/g, '-').toLowerCase();
+    var sections = document.getElementsByClassName("content");
+    for (let i = 0; i<sections.length; i++){
+        sections[i].style.display = "none";
+        sections[i].style.height = "0%";
+    }
+    document.getElementById(`${id}`).style.display = "block";
+    document.getElementById(`home-page`).style.height = "100%";
+}
